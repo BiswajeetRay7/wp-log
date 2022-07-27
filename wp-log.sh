07b79eb30e1d9d0352f5b28d7ee7798176c13661
Biswajeet Ray <94063930+BiswajeetRay7@users.noreply.github.com>
2022-07-28 04:29:23 +0530

    Update wp-log.sh

--- wp-log.sh
+++ wp-log.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
+cat << "EOF" | lolcat
+
+ __       __  _______           __        ______    ______  
+|  \  _  |  \|       \         |  \      /      \  /      \ 
+| $$ / \ | $$| $$$$$$$\        | $$     |  $$$$$$\|  $$$$$$\
+| $$/  $\| $$| $$__/ $$ ______ | $$     | $$  | $$| $$ __\$$
+| $$  $$$\ $$| $$    $$|      \| $$     | $$  | $$| $$|    \
+| $$ $$\$$\$$| $$$$$$$  \$$$$$$| $$     | $$  | $$| $$ \$$$$
+| $$$$  \$$$$| $$              | $$_____| $$__/ $$| $$__| $$
+| $$$    \$$$| $$              | $$     \\$$    $$ \$$    $$
+ \$$      \$$ \$$               \$$$$$$$$ \$$$$$$   \$$$$$$ 
+                                                            
+                                                            
+                                                            
+
+                          Author: Biswajeet Ray
+                     Github:github.com/BiswajeetRay7
+EOF
+
+LISTS=$1
+
+if [[ ! -f ${LISTS} ]]; then
+        echo "ERROR: ${LISTS} not found"
+        echo "usage: ./wp-log list.txt"
+        exit
+fi
+
+for SITE in $(cat $LISTS);
+do
+    if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${SITE}/wp-content/debug.log" ) =~ 'PHP Notice: ' ]]; then
+                echo -e "\e[32m[+] FOUND: ${SITE}/wp-content/debug.log"
+        else
+                echo -e "\e[31m[-] NOT FOUND: ${SITE}"
+        fi
+    
+done
 
